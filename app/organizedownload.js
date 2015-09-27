cd5c4c7a05437cab17f701c50c83edb0ec81ac65
timchap92 <tim@aardvark.com.au>
2015-09-26 17:25:58 -0700

    Added beginnings of setting storage functionality

--- app/organizedownload.js
+++ app/organizedownload.js
@@ -1,7 +1,43 @@
+
+chrome.downloads.onDeterminingFilename.addListener(function (item, __suggest) {
+    console.log('Determing filename.');
+    console.log('Referrer is: ' + item.referrer);
+
+    var domain = extractDomain(item.referrer);
+    console.log('Domain is: ' + domain);
+
+    //Does domain have an alias?
+    chrome.storage.local.get(domain, function (aliasObject) {
+
+        var folder;
+
+        if (aliasObject) {
+            console.log('Alias exists: ' + aliasObject.alias);
+            folder = aliasObject.alias;
+        } else {
+            console.log('No alias exists');
+            folder = domain;
+        }
+
+        //Generate filepath
+        var filepath = folder + '/' + item.filename;
+
+        //TODO: Sanitize filepath.
+
+        //Suggest filename for this download.
+        console.log('Suggesting filepath: ' + filepath);
+        __suggest({ filename: filepath });
+
+    });
+
+    return true;
+
+});
+
 function extractDomain(url) {
     var domain;
     //Find & remove protocol (http, ftp, etc.) and get domain
-    if (url.indexOf("://") > -1) {
+    if (url.indexOf('://') > -1) {
         domain = url.split('/')[2];
     }
     else {
@@ -19,22 +55,30 @@ function extractDomain(url) {
     return domain;
 }
 
-console.log("JavaScript loaded.");
+//Stores the alias object
+function storeAliasObject(string, obj) {
+    var key = string;
+    var value = obj;
+    var jsonfile = {};
+    jsonfile[key] = value;
+    chrome.storage.local.set(jsonfile, function () {
 
-chrome.downloads.onDeterminingFilename.addListener(function (item, __suggest) {
-    console.log("Determing filename.");
-    console.log("Referrer is: " + item.referrer);
+    });
 
-    var domain = extractDomain(item.referrer);
-    console.log("Domain is: " + domain);
+}
 
-    //Generate filepath
-    var filepath = domain + "/" + item.filename;
+//Retrieves the alias object
+function getAliasObjectAndSuggest(string) {
+    chrome.storage.local.get(string, function (obj) {
+        
+        var filepath;
 
-    //TODO: Sanitize filepath.
+        if (obj) {
+            //Alias exists for this domain
+            calculateFilepath(obj);
+        } else {
+            //No alias exists, use default option
 
-    //Suggest filename for this download.
-    console.log("Suggesting filepath: " + filepath);
-    __suggest({ filename: filepath });
-
-});
+        }
+    });
+}
