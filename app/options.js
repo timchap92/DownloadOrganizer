9fda97e64c21619c03f6c9cbbf0b294b27616f7c
timchap92 <tim@aardvark.com.au>
2015-09-29 17:42:14 -0700

    Added functionality
    Now iteratively looks for aliases in local chrome storage.

--- app/options.js
+++ app/options.js
@@ -1,31 +1,69 @@
 ﻿
 // Saves options to chrome.storage.local
 function save() {
-    alert('Hello!');
     console.log('Saving settings')
     var domain = document.getElementById('domain').value;
     var alias = document.getElementById('alias').value;
     var nest = document.getElementById('nest').checked;
-    chrome.storage.local.set({
-        domain: {alias: alias, nest: nest}
-    }, function () {
+    var jsonObject = {};
+    jsonObject[domain] = {alias: alias, nest: nest};
+    chrome.storage.local.set(jsonObject, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
-        console.log('Settings saved')
+        
+        chrome.storage.local.get(domain, function (item) {
+            status.textContent = item[domain].alias;
+        });
+        
+        initialize();
     });
 }
 
 
 
-// Clears all fields
-function restore() {
+// Initializes the options form
+function initialize() {
+    //Clear all fields
     document.getElementById('domain').value = "";
     document.getElementById('alias').value = "";
+    document.getElementById('nest').checked = false;
+
+    //Fill in existing aliases
+    chrome.storage.local.get(null, function (items) {
+        var data = document.getElementById('data');
+        var keys = Object.keys(items);
+        var n = keys.length;
+        var text = 'Data: (' + n + ' items)<br />Items: ' + items + '<br />';
+
+        data.textContent = text;
+
+        for (i = 0; i < n; i++) {
+            var domain = keys[i];
+            var item = items[keys[i]];
+            var alias = item.alias;
+
+            text += domain + ' --> ' + alias + '<br />';
+            
+        }
+
+        
+        data.innerHTML = text;
+
+    });
+
 }
-document.addEventListener('DOMContentLoaded', restore);
-document.getElementById('save').addEventListener('click',
-    save);
+
+function clear() {
+    chrome.storage.local.clear(function () {
+        initialize();
+
+    });
+
+    
+
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
+document.getElementById('save').addEventListener('click', save);
+document.getElementById('clear').addEventListener('click', clear);
--- app/organizedownload.js
+++ app/organizedownload.js
@@ -1,58 +1,174 @@
 
 chrome.downloads.onDeterminingFilename.addListener(function (item, __suggest) {
-    console.log('Determing filename.');
-    console.log('Referrer is: ' + item.referrer);
+    console.log('Download triggered.');
 
-    var domain = extractDomain(item.referrer);
-    console.log('Domain is: ' + domain);
+    var originalUrl = item.referrer;
+    console.log('Referrer is: ' + originalUrl);
 
-    //Does domain have an alias?
-    chrome.storage.local.get(domain, function (aliasObject) {
+    var originalUrlObject = parseUrl(originalUrl);
 
-        var folder;
-
-        if (aliasObject) {
-            console.log('Alias exists: ' + aliasObject.alias);
-            folder = aliasObject.alias;
-        } else {
-            console.log('No alias exists');
-            folder = domain;
-        }
-
-        //Generate filepath
-        var filepath = folder + '/' + item.filename;
-
-        //TODO: Sanitize filepath.
-
-        //Suggest filename for this download.
-        console.log('Suggesting filepath: ' + filepath);
-        __suggest({ filename: filepath });
-
-    });
+    iterativelyFindExistingAliasAndSuggest(originalUrlObject);
 
     return true;
 
+    function iterativelyFindExistingAliasAndSuggest(urlObject)
+    {
+        var url = convertToString(urlObject);
+
+        console.log('Searching for record matching: ' + url);
+        chrome.storage.local.get(url, function (aliasObject)
+        {
+
+            if (Object.keys(aliasObject).length != 0)
+            {
+                console.log('Found item: ' + JSON.stringify(aliasObject, null, 4));
+                var alias = aliasObject[url].alias;
+                console.log('Found alias: ' + alias);
+
+                suggestFilepath(alias, item.filename);
+
+            } else
+            {
+                console.log('No item found. Reducing specification.');
+                urlObject = reduceDomain(urlObject);
+                if (urlObject != null)
+                {
+                    iterativelyFindExistingAliasAndSuggest(urlObject);
+                } else
+                {
+                    console.log('Cannot reduce specification any further. Calculating default path.');
+                    suggestFilepath(getDefaultFolderPath(originalUrlObject), item.filename);
+                }
+            }
+        });
+    }
+
+    //Constructs the full filepath and uses to passed function to proceed with the download
+    function suggestFilepath(parentPath, filename)
+    {
+        var filepath = parentPath + '/' + filename;
+        console.log('Downloading to path: ' + filepath);
+
+        __suggest({ filename: filepath });
+    }
+        
+        
 });
 
-function extractDomain(url) {
+
+
+
+//Finds a 'less specified' domain for the given domain object
+function reduceDomain(urlObject)
+{
+    var path = urlObject.path;
+    var length = path.length;
+
+    if (length > 0)
+    {
+        //Reduce path
+        urlObject.path = path.slice(0, length - 1);
+    } else
+    {
+        if (urlObject.subdomain != '')
+        {
+            //Remove subdomain
+            urlObject.subdomain = '';
+        } else
+        {
+            urlObject = null;
+        }
+    }
+
+    return urlObject;
+
+}
+
+//Calculates the default folder path for this domain object
+function getDefaultFolderPath(urlObject) {
+    //TODO change this based on user preference
+    //return convertToString(urlObject);
+    return urlObject.domain;
+}
+
+//Converts a domain object to its string representation (i.e. URL)
+function convertToString(urlObject) {
+    var pathString = '';
+    var path = urlObject.path;
+    var pathLength = urlObject.path.length;
+
+    for (i = 0; i < pathLength; i++) {
+        pathString += '/' + path[i];
+    }
+
+    var result = (urlObject.domain + '.' +
+        urlObject.extension)
+
+    //Prepend the subdomain, if one exists
+    var subdomain = urlObject.subdomain;
+    if (subdomain != '') {
+        result = subdomain + '.' + result;
+    }
+
+    //Append the path, if one exists
+    if (pathString != '') {
+        result = result + pathString;
+    }
+
+    return result;
+        
+}
+
+//Extracts a domain object from the passed URL strings
+function parseUrl(url) {
+    
+    var subdomain
     var domain;
+    var extension;
+    var path;
+
     //Find & remove protocol (http, ftp, etc.) and get domain
     if (url.indexOf('://') > -1) {
-        domain = url.split('/')[2];
+        url = url.split('/')
+        path = url.slice(3, url.length);
+        url = url[2];
     }
     else {
-        domain = url.split('/')[0];
+        url = url.split('/');
+        path = url.slice(1, url.length);
+        url = url[0];
     }
 
+    console.log(path);
+
     //Find & remove port number
-    domain = domain.split(':')[0];
+    url = url.split(':')[0];
 
     //TODO: Fix this
-    domain = domain.split('\.');
+    url = url.split('\.');
 
-    domain = domain[domain.length - 2];
+    var length = url.length;
 
-    return domain;
+    extension = url[length - 1];
+    domain = url[length - 2];
+
+    if (length > 2) {
+        subdomain = url[url.length - 3];
+    } else {
+        subdomain = "";
+    }
+
+    urlObject = {
+        subdomain: subdomain,
+        domain: domain,
+        extension: extension,
+        path: path
+    };
+
+    console.log('Parsed URL: ' +
+        JSON.stringify(urlObject, null, 4));
+
+    return urlObject;
 }
 
 //Stores the alias object
